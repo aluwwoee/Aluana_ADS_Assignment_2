7f06fd3e5736e1c70479ca3fcd192b1480410d9c
banic <banichban186@gmail.com>
2025-04-14 22:24:16 +0500

    Added main

--- src/Main.java
+++ src/Main.java
@@ -14,17 +14,6 @@ public class Main {
         heap.insert(5);
         heap.insert(3);
         heap.insert(8);
-        System.out.println("Heap min: " + heap.getMin());
-        System.out.println("Heap extractMin: " + heap.extractMin());
-
-        MyList<String> arrayList = new MyArrayList<>();
-        arrayList.add("Hello");
-        arrayList.add("World");
-        System.out.println("ArrayList get(1): " + arrayList.get(1));
-
-        MyList<Integer> linkedList = new MyLinkedList<>();
-        linkedList.add(100);
-        linkedList.add(200);
-        System.out.println("LinkedList get(0): " + linkedList.get(0));
+        System.out.println("Heap min: " + heap.extractMin());
     }
 }
